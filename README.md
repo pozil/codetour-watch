33643d24b0f1b5e38286ac98d16e0f7296a4bffa
pozil <philippe.ozil@gmail.com>
2020-09-07 16:38:27 +0200

    Added more i/o and docs

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# GitHub action that flags file changes that may affect CodeTour content
+# CodeTour Watch
+
+A GitHub action that flags file changes in a PR that may affect [CodeTour content](https://marketplace.visualstudio.com/items?itemName=vsls-contrib.codetour).
+
+The action comments on the PR to report changes that impact CodeTour.
+
+The action will not comment the PR if changes do not impact CodeTour.
+
+## Usage
+
+```yml
+name: CodeTour watch
+
+on:
+    pull_request:
+        types: [opened, edited, synchronize, reopened]
+
+jobs:
+    codetour-watch:
+        runs-on: ubuntu-latest
+        steps:
+            - name: 'Watch CodeTour changes'
+              uses: pozil/codetour-watch@v1
+              with:
+                  repo-token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+## Inputs
+
+| Name     | Description                                         | Default |
+| -------- | --------------------------------------------------- | ------- |
+| `silent` | Optional flag that disables the commment on the PR. | `false` |
+
+## Outputs
+
+| Name                 | Description                                                                          |
+| -------------------- | ------------------------------------------------------------------------------------ |
+| `impactedFiles`      | The list of files covered by tours that were changed.                                |
+| `impactedTours`      | The list of tours that were impacted by the PR.                                      |
+| `missingTourUpdates` | The list of tours that were impacted by the changes but that are not part of the PR. |
--- src/index.js
+++ src/index.js
@@ -1,56 +1,160 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
+
+const TOUR_DIR = '.tours/';
 
 const run = async () => {
-    // Get octokit
-    const gitHubToken = core.getInput('repo-token', { required: true });
-    //const octokit = new github.GitHub(gitHubToken);
-
-    // Get repo and issue info
-    console.log(github.context.payload);
-
-    console.log(JSON.stringify(github.context.payload));
-
-    /*
-    const { repository, pull_number } = github.context.payload;
-    if (!pull_number) {
-        throw new Error(`Couldn't find issue info in current context`);
-    }
-    const [owner, repo] = repository.full_name.split('/');
-
-    //const compare = await octokit.Repository.Commit.Compare(owner, repo, 'master', 'branch');
-
-    const prFiles = octokit.pulls.listFiles({
-        owner,
-        repo,
-        pull_number,
-    });
-
-    // Get issue assignees
-    const assigneesString = core.getInput('assignees', { required: true });
-    const assignees = assigneesString
-        .split(',')
-        .map((assigneeName) => assigneeName.trim());
-
-    // Assign issue
-    console.log(
-        `Assigning issue ${issue.number} to users ${JSON.stringify(assignees)}`
-    );
     try {
-        await octokit.issues.addAssignees({
+        // Get inputs
+        const gitHubToken = core.getInput('repo-token', { required: true });
+        const isSilentMode =
+            core.getInput('silent') &&
+            core.getInput('silent').toLowerCase() === 'true';
+
+        // Get octokit
+        const octokit = github.getOctokit(gitHubToken);
+
+        // Get repo and PR info
+        const { repository, number, pull_request } = github.context.payload;
+        if (!pull_request) {
+            throw new Error(`Couldn't find PR info in current context`);
+        }
+        const [owner, repo] = repository.full_name.split('/');
+
+        // Get PR changed files
+        const prFiles = await getPrFiles(octokit, {
             owner,
             repo,
-            issue_number: issue.number,
-            assignees
+            pull_number: number
         });
+
+        // Parse CodeTour definitions
+        const tourDefinitions = await loadCodetours();
+
+        // Get files covered by CodeTour
+        const touredFiles = getFilesCoveredByCodetour(tourDefinitions);
+
+        // Get CodeTour files modified in PR
+        const impactedFiles = prFiles
+            .filter((file) => touredFiles.indexOf(file) !== -1)
+            .sort();
+
+        // Get CodeTour files that are impacted by update
+        const impactedTours = getCodetourFromFiles(
+            tourDefinitions,
+            impactedFiles
+        );
+
+        // Find impacted CodeTour files that are not updated in PR
+        const missingTourUpdates = [];
+        impactedTours.forEach((impactedTour) => {
+            if (prFiles.indexOf(impactedTour) === -1) {
+                missingTourUpdates.push(impactedTour);
+            }
+        });
+
+        // Comment PR if action is not silenced and a CodeTour is affected
+        if (!isSilentMode && impactedTours.length > 0) {
+            await commentPr(
+                octokit,
+                {
+                    owner,
+                    repo,
+                    issue_number: number
+                },
+                impactedFiles,
+                impactedTours,
+                missingTourUpdates
+            );
+        }
+
+        // Set action ouput
+        core.setOutput('impactedFiles', impactedFiles);
+        core.setOutput('impactedTours', impactedTours);
+        core.setOutput('missingTourUpdates', missingTourUpdates);
     } catch (error) {
+        console.error(error);
         core.setFailed(error.message);
     }
-*/
 };
 
-try {
-    run();
-} catch (error) {
-    core.setFailed(error.message);
-}
+const commentPr = async (
+    octokit,
+    commentInfo,
+    impactedFiles,
+    impactedTours,
+    missingTourUpdates
+) => {
+    let body;
+    if (missingTourUpdates > 0) {
+        body += '⚠️ ';
+    }
+    body += `**CodeTour Watch**\n\n
+Changed files with CodeTour:\n\n`;
+    impactedFiles.forEach((file) => {
+        body += `- \`${file}\`\n`;
+    });
+    body += '\nImpacted CodeTour files:\n\n';
+    impactedTours.forEach((tour) => {
+        if (missingTourUpdates.indexOf(tour) === -1) {
+            body += `- \`${tour}\`\n`;
+        } else {
+            body += `- \`${tour}\` - ⚠️ **Missing from PR**\n`;
+        }
+    });
+    body += `\nMake sure to review CodeTours and update line numbers accordingly.`;
+    commentInfo.body = body;
+
+    await octokit.issues.createComment(commentInfo);
+};
+
+const getPrFiles = async (octokit, prInfo) => {
+    try {
+        const prDetails = await octokit.pulls.listFiles(prInfo);
+        return prDetails.data.map((file) => file.filename);
+    } catch (error) {
+        throw new Error(`Failed to retrieved PR files: ${error}`);
+    }
+};
+
+const getCodetourFromFiles = (tourDefinitions, files) => {
+    const defFiles = new Set();
+    tourDefinitions.forEach((tourDefinition) => {
+        tourDefinition.steps.forEach((step) => {
+            if (files.indexOf(step.file) !== -1) {
+                defFiles.add(tourDefinition.filename);
+            }
+        });
+    });
+    return Array.from(defFiles);
+};
+
+const loadCodetours = async () => {
+    // List CodeTour definition files
+    let defFiles;
+    try {
+        defFiles = await fs.promises.readdir(TOUR_DIR);
+    } catch (error) {
+        throw new Error(`Failed to read ${TOUR_DIR} directory: ${error}`);
+    }
+    // Parse CodeTour definitions
+    return defFiles.map((defFile) => {
+        const path = `${TOUR_DIR}${defFile}`;
+        const definition = JSON.parse(fs.readFileSync(path, 'utf-8'));
+        definition.filename = path;
+        return definition;
+    });
+};
+
+const getFilesCoveredByCodetour = (tourDefinitions) => {
+    const touredFiles = new Set();
+    tourDefinitions.forEach((tourDefinition) => {
+        tourDefinition.steps.forEach((step) => {
+            touredFiles.add(step.file);
+        });
+    });
+    return Array.from(touredFiles);
+};
+
+run();
